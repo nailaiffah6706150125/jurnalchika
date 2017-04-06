f0e2a3474f492e4154b6fab518795649898ac3e4
marshachikita6706151145 <marshachikita16@gmail.com>
2017-04-06 08:29:58 +0700

    Add files via upload

---
+++ Jurnal/src/jurnal/Jurnal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Jurnal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+        int c = 1;
+        int data;
+        
+        System.out.print("Input: ");
+        Scanner show = new Scanner(System.in);
+        data = show.nextInt();
+        
+        for(int i = 1; i <= data; i++){
+            c = a+b; 
+            System.out.print(c+" ");
+            a = b;
+            b = c;
+        }
+        
+    }
+    
+}
